7186255c319648aa18ba17eaf9199d2b89d6f562
Davi Barreto <daviarturdesa686@gmail.com>
2023-11-26 21:11:08 -0300

    add interceptors

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,4 +4,11 @@ const api = axios.create({
   baseURL: 'http://192.168.1.12:3333'
 })
 
+api.interceptors.response.use((response) => {
+  return response
+}, (error) => {
+  console.log(error)
+  return {}
+})
+
 export default api
